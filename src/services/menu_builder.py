6eb220561d630465eb7eeca3471d0ea113ec7ed4
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-04-22 22:08:17 +0200

    :white_check_mark: Chore: Add 'get_main_menu' function in MenuBuilder class to create a menu from an csv file.

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
+from models.ingredient import Restriction
 
 DATA_PATH = "data/menu_base_data.csv"
 INVENTORY_PATH = "data/inventory_base_data.csv"
@@ -26,4 +27,25 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        menu = {
+            "dish_name": [],
+            "ingredients": [],
+            "price": [],
+            "restrictions": [],
+        }
+
+        for dish_data in self.menu_data.dishes:
+            dish_restrictions_list = dish_data.get_restrictions()
+            menu_has_restriction = False
+            for restriction_data in dish_restrictions_list:
+                if restriction_data == restriction:
+                    menu_has_restriction = not menu_has_restriction
+
+            if not menu_has_restriction:
+                menu["dish_name"].append(dish_data.name)
+                menu["ingredients"].append(dish_data.get_ingredients())
+                menu["price"].append(dish_data.price)
+                menu["restrictions"].append(dish_restrictions_list)
+
+        df = pd.DataFrame(menu)
+        return df
