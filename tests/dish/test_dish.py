9b6a6eb62e246f5aaaca3a0e9c889c4fcdf4f508
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-04-22 16:03:25 +0200

    :white_check_mark: Chore: Add 'open_csv_file' function in MenuData class to read a csv file and add the dishes in the attribute 'dishes'

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -15,17 +15,25 @@ def test_dish():
     assert grilled_bread.__eq__(grilled_bread) == True
     assert grilled_bread.__eq__(yakisoba) == False
     assert grilled_bread.__hash__() == hash("Dish('pão na chapa', R$2.25)")
-    
-    grilled_bread.add_ingredient_dependency(Ingredient('pão'), 1)
-    grilled_bread.add_ingredient_dependency(Ingredient('manteiga'), 1)
-    
-    assert grilled_bread.recipe == {Ingredient('pão'): 1, Ingredient('manteiga'): 1}
+
+    grilled_bread.add_ingredient_dependency(Ingredient("pão"), 1)
+    grilled_bread.add_ingredient_dependency(Ingredient("manteiga"), 1)
+
+    assert grilled_bread.recipe == {
+        Ingredient("pão"): 1,
+        Ingredient("manteiga"): 1,
+    }
     assert len(grilled_bread.get_restrictions()) == 2
-    
-    assert grilled_bread.get_ingredients() == {Ingredient('pão'), Ingredient('manteiga')}
-    
+
+    assert grilled_bread.get_ingredients() == {
+        Ingredient("pão"),
+        Ingredient("manteiga"),
+    }
+
     with pytest.raises(TypeError, match="Dish price must be float."):
         Dish("pão na chapa", "2.25")
-        
-    with pytest.raises(ValueError, match="Dish price must be greater then zero."):
+
+    with pytest.raises(
+        ValueError, match="Dish price must be greater then zero."
+    ):
         Dish("pão na chapa", -2.25)
